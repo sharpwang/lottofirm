2434f1c948affd78f5fc974b4347ae7209227bc4
sharpwang@live.com <sharpwang@live.com>
2014-10-16 15:38:19 +0800

    make it a module

--- dlt17500.py
+++ dlt17500.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import Column, String, Integer, create_engine, exc
@@ -52,80 +53,81 @@ class Tdlt(Base):
     fjwfjj1 = Column(Integer) 
 
 
-# 初始化数据库连接:
-engine = create_engine('mysql+mysqlconnector://root:wang3@lottofirm.net:3306/lottofirm')
-# 创建DBSession类型:
-DBSession = sessionmaker(bind=engine)
-# 创建session对象:
-session = DBSession()
 
-response = urllib2.urlopen('http://www.17500.cn/getData/dlt.TXT', timeout=10)
-html = response.read()
-data = html.split('\n')
-for line in reversed(data):
-    record = line.split(' ')
-    if(len(record) > 7):
-        dlt = Tdlt()
-        dlt.qh      = record[0]
-        dlt.rq      = record[1]
-        dlt.qq1     = record[2]
-        dlt.qq2     = record[3]
-        dlt.qq3     = record[4]
-        dlt.qq4     = record[5]
-        dlt.qq5     = record[6]
-        dlt.hq1     = record[7]
-        dlt.hq2     = record[8]
-        dlt.tzze    = record[9] 
-        dlt.grxq    = record[10] 
-        dlt.zj1     = record[11] 
-        dlt.jj1     = record[12] 
-        dlt.zj2     = record[13] 
-        dlt.jj2     = record[14] 
-        dlt.zj3     = record[15] 
-        dlt.jj3     = record[16] 
-        dlt.zj4     = record[17] 
-        dlt.jj4     = record[18] 
-        dlt.zj5     = record[19] 
-        dlt.jj5     = record[20] 
-        dlt.zj6     = record[21] 
-        dlt.jj6     = record[22] 
-        dlt.zj7     = record[23] 
-        dlt.jj7     = record[24] 
-        dlt.zj8     = record[25] 
-        dlt.jj8     = record[26] 
-        dlt.zjzj1   = record[27] 
-        dlt.zjjj1   = record[28] 
-        dlt.zjzj2   = record[29] 
-        dlt.zjjj2   = record[30] 
-        dlt.zjzj3   = record[31] 
-        dlt.zjjj3   = record[32] 
-        dlt.fjwftze = record[33] 
-        dlt.fjwfzj1 = record[34] 
-        dlt.fjwfjj1 = record[35] 
-        session.add(dlt)
-        try:
-            session.commit()
-        except exc.IntegrityError:
-            #最新的一条都要插进去，或者更新
+
+def flash_data():
+    # 初始化数据库连接:
+    engine = create_engine('mysql+mysqlconnector://root:wang3@lottofirm.net:3306/lottofirm')
+    # 创建DBSession类型:
+    DBSession = sessionmaker(bind=engine)
+    # 创建session对象:
+    session = DBSession()
+
+    response = urllib2.urlopen('http://www.17500.cn/getData/dlt.TXT', timeout=10)
+    html = response.read()
+    data = html.split('\n')
+    for line in reversed(data):
+        record = line.split(' ')
+        if(len(record) > 7):
+            dlt = Tdlt()
+            dlt.qh      = record[0]
+            dlt.rq      = record[1]
+            dlt.qq1     = record[2]
+            dlt.qq2     = record[3]
+            dlt.qq3     = record[4]
+            dlt.qq4     = record[5]
+            dlt.qq5     = record[6]
+            dlt.hq1     = record[7]
+            dlt.hq2     = record[8]
+            dlt.tzze    = record[9] 
+            dlt.grxq    = record[10] 
+            dlt.zj1     = record[11] 
+            dlt.jj1     = record[12] 
+            dlt.zj2     = record[13] 
+            dlt.jj2     = record[14] 
+            dlt.zj3     = record[15] 
+            dlt.jj3     = record[16] 
+            dlt.zj4     = record[17] 
+            dlt.jj4     = record[18] 
+            dlt.zj5     = record[19] 
+            dlt.jj5     = record[20] 
+            dlt.zj6     = record[21] 
+            dlt.jj6     = record[22] 
+            dlt.zj7     = record[23] 
+            dlt.jj7     = record[24] 
+            dlt.zj8     = record[25] 
+            dlt.jj8     = record[26] 
+            dlt.zjzj1   = record[27] 
+            dlt.zjjj1   = record[28] 
+            dlt.zjzj2   = record[29] 
+            dlt.zjjj2   = record[30] 
+            dlt.zjzj3   = record[31] 
+            dlt.zjjj3   = record[32] 
+            dlt.fjwftze = record[33] 
+            dlt.fjwfzj1 = record[34] 
+            dlt.fjwfjj1 = record[35] 
+            session.add(dlt)
             try:
-                session.rollback()
-                session.merge(dlt)
                 session.commit()
-            except exc.SQLAlchemyError:
+            except exc.IntegrityError:
+                #最新的一条都要插进去，或者更新
+                try:
+                    session.rollback()
+                    session.merge(dlt)
+                    session.commit()
+                except exc.SQLAlchemyError:
+                    pass
+                #这里加上break就会只更新最后一条
+                break
+            else:
                 pass
-            #这里加上break就会只更新最后一条
-            break
-        else:
-            pass
 
-session.commit()
-session.close()
+    session.commit()
+    session.close()
 
 
-
-
-
-
+if __name__=='__main__':
+    flash_data()
 
 # 0 14120
 # 1 2014-10-13
